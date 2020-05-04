29ae35b22cab7d89796f0c5c4a3c172147d7d316
Robert Brignull <robertbrignull@gmail.com>
2020-05-04 18:28:01 +0100

    switch to using ava

--- src/external-queries.test.ts
+++ src/external-queries.test.ts
@@ -1,10 +1,11 @@
+import test from 'ava';
 import * as fs from "fs";
 import * as path from "path";
 
 import * as configUtils from "./config-utils";
 import * as externalQueries from "./external-queries";
 
-test("checkoutExternalQueries", async () => {
+test("checkoutExternalQueries", async t => {
     let config = new configUtils.Config();
     config.externalQueries = [
         new configUtils.ExternalQuery("github/codeql-go", "df4c6869212341b601005567381944ed90906b6b"),
@@ -13,5 +14,5 @@ test("checkoutExternalQueries", async () => {
 
     let destination = process.env["RUNNER_WORKSPACE"] || "/tmp/codeql-action/";
     // COPYRIGHT file existed in df4c6869212341b601005567381944ed90906b6b but not in master
-    expect(fs.existsSync(path.join(destination, "github", "codeql-go", "COPYRIGHT"))).toBeTruthy();
+    t.true(fs.existsSync(path.join(destination, "github", "codeql-go", "COPYRIGHT")));
 });
